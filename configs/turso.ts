d0160cbe505223c260d640b5378256e1abbaa9f4
delanyo agbenyo <delanyodevio@gmail.com>
2026-05-11 02:07:04 +0000

    Feat: switched to bun

--- configs/turso.ts
+++ configs/turso.ts
@@ -6,5 +6,9 @@ const TURSO_DATABASE_URL = ``;
 export default defineConfig({
   schema: "./src/db/schema/preps.ts",
   dialect: "turso",
+  // dbCredentials: {
+  //   url: process.env.DATABASE_URL,
+  //   authToken: process.env.DATABASE_AUTH_TOKEN,
+  // },
   dbCredentials: { url: TURSO_DATABASE_URL, authToken: TURSO_AUTH_TOKEN },
 });
